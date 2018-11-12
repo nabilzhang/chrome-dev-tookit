87b83821db957414c20dc476a913f2700ec12a3a
远山 <yuanshan@pinduoduo.com>
2018-11-13 00:44:34 +0800

    url encode

--- js/strings.js
+++ js/strings.js
@@ -3,3 +3,15 @@ $("#reverse-string").click(function(){
   var origin = $("#string").val();
   $("#reversed-string").val(origin.split("").reverse().join(""));
 });
+
+
+// URL encode and decode
+$("#url-encode-btn").click(function(){
+  var origin = $("#url-input").val();
+  $("#url-output").val(encodeURIComponent(origin));
+});
+
+$("#url-decode-btn").click(function(){
+  var origin = $("#url-input").val();
+  $("#url-output").val(decodeURIComponent(origin));
+});
